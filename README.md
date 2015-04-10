b4078187bdd2dfc5e010311865e1899b222158ee
bundgas <kml@bundgas.dk>
2015-04-11 01:29:14 +0200

    Changes to be committed: 	modified:   README.md 	modified:   changelog.txt 	modified:   zfs-replicator.sh

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Incremental ZFS replicator with grandfathering scheme support for FreeBSD and ZF
 
 USAGE (should be used with crontab): zfs-replicator.sh (generation-name) (number of snapshots to keep)
 
-This shell script recursively snapshots a given pool, and replicates it to another host (slave). It supports grandfathering schemes (generations) on both master and slave (mirrored), all controlled from the master with this script and crontab.
+This shell script recursively snapshots a given pool, and replicates it to another host (slave). It supports grandfathering schemes (generations) on both master and slave with mirrored or different keep rules, all controlled from the master with this script and crontab.
 
 Only the first snapshot will be sent in 100% size, all following snapshots will be sent incrementally.
 
@@ -15,7 +15,7 @@ If you are running multible generations, cleaning of the individual generation w
 
 This script was created because i needed something that worked on both FreeBSD and Linux (ZFS-on-Linux), and all the other scripts i tried failed, didn't work (on FreeBSD or Linux or both), were not suited, were overkill, or were too complicated for me to make work.
 
-This script has been worked on over time, and was started to just take snapshots one at a time and send them to a slave. I started out with a script i found here: http://www.aisecure.net/2012/01/11/automated-zfs-incremental-backups-over-ssh/ and worked on that. Over time a lot of stuff was added here and there, then was rewritten, moved around, more functionality added, more moving around, rewritten, and so on and so on. Therefore some logic might seem weird in some places, but a lot of it will take care of unexpected situations i ended up in, and now this has been running in production on some of my systems, both Linux (Ubuntu 12.04 and 14.04) and FreeBSD (10.0 and 10.1), and seem to be running error free. This script does not work on Solaris, and i haven't tested it on Illumos. Please comment on this if you test this on other systems.
+This script has been worked on over time, and was started to just take snapshots one at a time and send them to a slave. Over time a lot of stuff was added here and there, then was rewritten, moved around, more functionality added, more moving around, rewritten, and so on and so on. Therefore some logic might seem weird in some places, but a lot of it will take care of unexpected situations i ended up in, and now this has been running in production on some of my systems, both Linux (Ubuntu 12.04 and 14.04) and FreeBSD (10.0 and 10.1), and seem to be running error free. This script does not work on Solaris, and i haven't tested it on Illumos. Please comment on this if you test this on other systems.
 
 I'm a sysadmin, not a coder, so there will be some "sledgehammer" approches in the script, but it works. Please feel free to do with this what you want, and i hope this will make your life easier.
 
--- zfs-replicator.sh
+++ zfs-replicator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# ZFS replicator v. 0.9.2 - By Kenneth Lutzke - kml@bundgas.dk
+# ZFS replicator v. 0.9.3 - By Kenneth Lutzke - kml@bundgas.dk
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
 # <kml@bundgas.dk> wrote this file. As long as you retain this notice you
@@ -8,31 +8,37 @@
 # ----------------------------------------------------------------------------
 # DISCLAIMER: YOU RUN THIS AT YOUR OWN RISK!!! If you don't agree, don't run this script.
 # ----------------------------------------------------------------------------
-# USAGE (should be used with crontab): zfs-replicator.sh <generation-name> <number of snapshots to keep>
+# USAGE: zfs-replicator.sh <generation-name> <number of snapshots to keep on master> <*number of snapshots to keep on slave>
+# * optional. If not used, master keep rule will be mirrored.
+# This script is meant to be run by crontab. See examples below.
 #
 # This script recursively snapshots a given pool, and replicates it to another host (slave). It supports grandfathering schemes (generations)
-# on both master and slave (mirrored), all controlled from the master with this script and crontab.
+# on both master and slave with mirrored or different keep rules, all controlled from the master with this script and crontab.
 # Only the first snapshot will be sent in 100% size, all following snapshots will be sent incrementally.
 #
 # If the slave is down, or the script is already running (ex. in case of a big initial sync that takes time), a snapshot will still be taken,
 # and it will be synced with other missed snapshots when the slave is up and the script is not already running. While the slave is down or the
 # script is already running, no cleaning will be done on the master or the slave, so snapshots will build up on the master until the slave is
-# synced again. As soon as the master and slave are in sync again, cleaning will kick in and obey the keep rule(s).
+# synced again. As soon as the master and slave are in sync again, cleaning will kick in and obey the keep rule(s). The cleaning process only
+# cleans snapshots within its own generation.
 #
-# BE VERY CAREFUL WITH "zfs destroy -r":
-# If you destroy a filesystem on the master, it will destroy the filesystem on the slave too, including all the snapshots (across all generations)
-# for that filesystem, with a single run of the script. This is as intended, as it is the way recursive zfs send and receive work, and as it comes
-# in handy when you really want the data deleted forever. So if you need the snapshots kept, don't destroy the filesystem, delete the data inside
-# the filesystem instead, and destroy it when it has "trickeled through" your generations.
+# If you destroy a filesystem with 'zfs destroy -r' on the master, the filesystem and snapshots will still exist on the slave. The snapshots 
+# will eventually be destroyed in the cleaning process, but the filesystem and content will remain, and will need to be destroyed manually.
 #
-# You can create manual snapshots on the master, as long as you don't name your snapshot with the same prefix as in this script. If you create a
-# snapshots on the slave, it will be destroyed as soon as this script is run again. This is how ZFS recursive send/receive works...
+# You can create manual snapshots on both master and slave, as long as you don't name them with the same prefix as in this script.
+#
+# It is important that none of the filesystems are mounted on the slave, as any change there, would result in the next sync failing. You might
+# need to do a manual recursive rollback to the last successful snapshot on the slave, if you need to resync after a mount that changed something.
+# The script sets canmount=noauto on all receiving filesystems on the slave, to avoid them being mounted on boot. If this script finds a mounted
+# filesystem on the slave, a snapshot will be taken (but no sync) and the script will exit with an alert in the monitor output and info in the log.
+#
+# --------------------------------------------------------------------------------------------------------------------------------------------
 #
 # For this script to work, you need to do the following things:
-# - ssh keys with no passphrase must first be set up so that a user (ex. root) can ssh from the master to the slave without being prompted for login.
-# - create a pool with the same name as the master-pool, that you want replicated, on the slave.
+# - SSH keys with no passphrase must first be set up so that a user (ex. root) can ssh from the master to the slave without being prompted for login.
+# - Create a pool with the same name as the master-pool, that you want replicated, on the slave, and make sure to unmount it with 'zfs unmount'.
 #
-# Example of use with crontab (*1min schedule should only be used for testing, or on pools with few filesystems and low io).
+# Example 1 of use with crontab (*1min schedule should only be used for testing, or on pools with few filesystems and low io).
 #
 # 1-4/1,6-9/1,11-14/1,16-19/1,21-24/1,26-29/1,31-34/1,36-39/1,41-44/1,46-49/1,51-54/1,56-59/1 * * * *  root    /root/zfs-replicator.sh 1min 5
 # 5,10,20,25,35,40,50,55 * * * *           root   /root/zfs-replicator.sh 5min 3
@@ -42,11 +48,13 @@
 #
 # The example is set on FreeBSD. On Linux you might need to remove "root" in front of the command entry.
 # This would give you 1 snapshot every minute for 5 minutes, 1 snapshot every 5 minutes for 15 minutes, 1 snapshot every 15 minutes for 1 hour,
-# 1 snapshot every hour for 6 hours, and 1 snapshot every 6 hours for 2 days* (* 2 day = 8 snapshots. The 9 in the example is because you need
+# 1 snapshot every hour for 6 hours, and 1 snapshot every 6 hours for 2 days* (* 2 days = 8 snapshots. The 9 in the example is because you need
 # one extra snapshot in the end to be able to go 2 whole days back, or that snapshot will just have have been destroyed, and you could now only
 # go 1 day and 18 hours back. This is not a problem for 1min, 5min, 15min or 1hour schedules in the above example, as they all overlap 1 snapshot
 # with the next generation. Because of the way cleaning doesn't interfere with other generations, there will sometimes be leftover snapshots,
 # where the generations overlap, but they will be automatically cleaned up on the next run of the script with that generation.
+# As there are no crontab entries utilizing the slave keep rule in this example, the master and slave will keep the same snapshots, and thereby be
+# mirrored.
 #
 # * 1min schedule will most likely only work smoothly with very few filesystems with nearly no writing IO, as it can take some time to sync each 
 # filesystem. This would give you a lot of overlapping script-executions, and you will end up with a snapshot backlog, which would probably
@@ -54,14 +62,25 @@
 # transfer everything incrementally to the slave. Because the script picks up snapshots missed for transfer on the next round, it is ok if it
 # doesn't make it on time every time, but if your increments are set too often, you are probably in for a bumpy ride.
 #
+# Example 2 of use with crontab:
+#
+# 15,30,45 * * * *                         root   /root/zfs-replicator.sh 15min 7
+# 0 1-23 * * *                             root   /root/zfs-replicator.sh 1hour 24
+# 0 0 * * *                                root   /root/zfs-replicator.sh 1day 3 15
+#
+# The example is set on FreeBSD. On Linux you might need to remove "root" in front of the command entry.
+# This would give you 1 snapshot every 15 minutes for 2 hours, 1 snapshot every hour for 24 hours, and 1 snapshot every day for 2 days on the
+# master and 14 days on the slave. Read example 1 in regards to overlap and cleanup as it also applies to this example.
+#
+# --------------------------------------------------------------------------------------------------------------------------------------------
+#
 # The script outputs a log and monitor output. The monitor output can be used for monitoring software such as Nagios.
-# It uses arcfour128 as ssh cipher. 
+# It uses arcfour128 as ssh cipher for transferring snapshots.
 #
 # If you want to be extra sure that you get monitor-output if the script fails, run it in crontab like this:
 # 15,30,45 * * * *  root   /root/zfs-replicator.sh 15min 4 || echo "CRITICAL - Sync script failed - Check manually!!!" >> /path/to/monitor-file
-# ----------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------
 
-sleep 1
 PATH=/usr/bin:/sbin:/bin
 
 ##### CONFIG #####
@@ -125,7 +144,14 @@ if [ -z "$2" ]; then
 fi
 
 snapname="$1"
-keep="$2"
+masterkeep="$2"
+
+if [ -z "$3" ]; then
+ echo "`date +"%Y-%m-%d %H:%M:%S"` - No slave keep rule defined. Mirroring master rule." >> $logfile
+ slavekeep="$masterkeep"
+else
+ slavekeep="$3"
+fi
 
 echo "`date +"%Y-%m-%d %H:%M:%S"` - Script started - Checks are OK" >> $logfile
 
@@ -160,6 +186,11 @@ if [ -f $lockfile ]; then
  exit 0
 fi
 
+ if ! touch $lockfile; then
+  echo "Cannot create lock file $lockfile - Please check your config." >> $logfile
+  exit 1
+ fi
+
 touch $lockfile
 
 # Check if slave is online
@@ -185,12 +216,30 @@ fi
 # Include last successful sync snapshot variable
 . $lastsucclog
 
+# Check if there are mounted filesystems on the slave
+if [ `ssh $user@$host zfs get mounted | grep "^$pool[ /]" | grep -v "@" | grep " yes " | wc -l` -ne "0" ]; then
+ echo "`date +"%Y-%m-%d %H:%M:%S"` - The following filesystem(s) is/are mounted on the slave: `ssh $user@$host zfs get mounted | grep "^$pool[ /]" | grep -v "@" | grep " yes " | cut -f1 -d" " | tr '\n' ' '`. Please unmount the filesystem(s) manually. If the script fails to sync after unmounting all the filesystems, you might need to do a recursive rollback to the last successful snapshot on the slave. No sync or cleanup this round." >> $logfile
+ echo "$monitor_critical_prefix filesystem(s) mounted on slave. See logfile on master and unmount filesystem(s) manually on the slave. Snapshot taken, but no sync or cleanup this round." > $monitor_output
+ rm $lockfile
+ echo "`date +"%Y-%m-%d %H:%M:%S"` - exiting" >> $logfile
+ exit 0
+fi
+
 # Transfer snapshot(s) to slave
 if [ -z "$lastsucc" ]; then
  echo "`date +"%Y-%m-%d %H:%M:%S"` - No last successful snapshot - syncing initial snapshot $snapshot_now" >> $logfile
  if zfs send -R $snapshot_now | ssh -c arcfour128 $user@$host zfs receive -Fduv $pool >> $logfile; then
   echo "lastsucc=$snapshot_now" > $lastsucclog
   echo "`date +"%Y-%m-%d %H:%M:%S"` - Initial snapshot $snapshot_now was successfully synced to $host . No cleanup on this round." >> $logfile
+  # First check and set slave-filesystems to option canmount=noauto
+  if echo "for setnoauto in \$(for fs in \$(zfs list -H -o name -t snapshot | grep -e \"^$pool[ @/]\" | cut -f1 -d\"@\" | uniq) ; do zfs get canmount \$fs ; done | grep canmount | grep -v \" noauto \" | cut -f1 -d\" \") ; do zfs set canmount=noauto \$setnoauto ; done" | ssh $user@$host /bin/sh ; then
+   echo "`date +"%Y-%m-%d %H:%M:%S"` - Checked option on slave filesystems (canmount=noauto)" >> $logfile
+  else
+   echo "`date +"%Y-%m-%d %H:%M:%S"` - Could not check or set option on slave filesystems (canmount=noauto) - No cleanup this round. Check slave filesystems manually if this persists." >> $logfile
+   echo "$monitor_warn_prefix Could not check or set option on slave filesystems (canmount=noauto) - No cleanup this round. Check slave filesystems manually if this persists." > $monitor_output
+   echo "`date +"%Y-%m-%d %H:%M:%S"` - exiting" >> $logfile
+   exit 0
+  fi
   echo "`date +"%Y-%m-%d %H:%M:%S"` - exiting" >> $logfile
   echo -n "" > $monitor_output
   rm $lockfile
@@ -211,7 +260,7 @@ else
     echo "`date +"%Y-%m-%d %H:%M:%S"` - snapshots have been taken by this script after $lastsucc that were not synced. Syncing those first" >> $logfile
     while [ `zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-" | grep -A 2 -e "^$lastsucc$" | wc -l` -gt "2" ]; do
      missedsnap=`zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-" | grep -A 1 -e "^$lastsucc$"  | tail -n 1`
-     if zfs send -R -i $lastsucc $missedsnap | ssh -c arcfour128 $user@$host zfs receive -Fduv $pool >> $logfile; then 
+     if zfs send -R -i $lastsucc $missedsnap | ssh -c arcfour128 $user@$host zfs receive -duv $pool >> $logfile; then 
       echo "`date +"%Y-%m-%d %H:%M:%S"` - Missed snapshot $missedsnap successfully synced to $host ." >> $logfile
       echo "lastsucc=$missedsnap" > $lastsucclog
       lastsucc=$missedsnap
@@ -225,7 +274,7 @@ else
     done
    fi
    if [ $lastsucc != $snapshot_now ] ; then
-    if zfs send -R -i $lastsucc $snapshot_now | ssh -c arcfour128 $user@$host zfs receive -Fduv $pool >> $logfile; then
+    if zfs send -R -i $lastsucc $snapshot_now | ssh -c arcfour128 $user@$host zfs receive -duv $pool >> $logfile; then
      echo "`date +"%Y-%m-%d %H:%M:%S"` - $snapshot_now successfully synced to $host ." >> $logfile
      echo "lastsucc=$snapshot_now" > $lastsucclog
     else
@@ -254,9 +303,18 @@ fi
 
 lastsucc=$snapshot_now
 
+# Second check and set slave-filesystems to option canmount=noauto
+if echo "for setnoauto in \$(for fs in \$(zfs list -H -o name -t snapshot | grep -e \"^$pool[ @/]\" | cut -f1 -d\"@\" | uniq) ; do zfs get canmount \$fs ; done | grep canmount | grep -v \" noauto \" | cut -f1 -d\" \") ; do zfs set canmount=noauto \$setnoauto ; done" | ssh $user@$host /bin/sh ; then
+ echo "`date +"%Y-%m-%d %H:%M:%S"` - Checked option on slave filesystems (canmount=noauto)" >> $logfile
+else
+ echo "`date +"%Y-%m-%d %H:%M:%S"` - Could not check or set option on slave filesystems (canmount=noauto) - No cleanup this round. Check slave filesystems manually if this persists." >> $logfile
+ echo "$monitor_warn_prefix Could not check or set option on slave filesystems (canmount=noauto) - No cleanup this round. Check slave filesystems manually if this persists." > $monitor_output
+ exit 0
+fi
+
 # snapshot cleaning
 # master
-while [ `zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-$snapname-" | wc -l` -gt "$(( $keep ))" ]; do
+while [ `zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-$snapname-" | wc -l` -gt "$(( $masterkeep ))" ]; do
  cleanmaster=`zfs list -H -o name -t snapshot | sort | grep -e "^$pool@$prefix-$snapname-" | head -n 1`
  if [ $lastsucc != $cleanmaster ] ; then
   if zfs destroy -r $cleanmaster >> $logfile ; then
@@ -278,7 +336,7 @@ while [ `zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-$snapname-" |
 done
 
 # slave
-while [ `ssh $user@$host zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-$snapname-" | wc -l` -gt "$(( $keep ))" ]; do
+while [ `ssh $user@$host zfs list -H -o name -t snapshot | grep -e "^$pool@$prefix-$snapname-" | wc -l` -gt "$(( $slavekeep ))" ]; do
  cleanslave=`ssh $user@$host zfs list -H -o name -t snapshot | sort | grep -e "^$pool@$prefix-$snapname-" | head -n 1`
  if [ $lastsucc != $cleanslave ] ; then
   if ssh $user@$host zfs destroy -r $cleanslave >> $logfile ; then
